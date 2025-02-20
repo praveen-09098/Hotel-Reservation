e8fc625448da687d6e41f5e37cab0d3b9c7a442e
sri sai praveeen <114608025+praveen-09098@users.noreply.github.com>
2025-02-19 22:23:03 -0600

    Update assetLogin.js

--- assetLogin.js
+++ assetLogin.js
@@ -26,6 +26,7 @@ function loginUser() {
             }, error: function (error) {
 				alert("Wrong username or password");
 		    //wniuiuidbiwudwiydwdbyibibwiybyiybwy
+		    //wsnusnownsuwusnounosowsnwusowsnosnws
 		
 				               
             }
